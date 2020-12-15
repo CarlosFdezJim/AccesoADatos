0c2958bf25b226679fae19f225748ccb47bbd1f8
CarlosFdezJim <57524609+CarlosFdezJim@users.noreply.github.com>
2020-12-15 10:28:34 +0100

    Acceso a datos con ANT

---
+++ AccesoADatosFicheroANT/src/Modelo/Liga.java
@@ -0,0 +1,86 @@
+/**
+ * @author Fernández
+ */
+
+package Modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class Liga implements Serializable {
+
+    private String ID_Liga = "";
+    private String Nombre_Liga = "";
+    private String Num_Equipos = "";
+    private String Num_Ligas = "";
+    private String Federacion = "";
+
+    public Liga() {
+    }
+
+    public Liga(String ID,String Nombre, String Numero_Equipos, String Numero_ligas, String Federacion){
+        this.ID_Liga = ID;
+        this.Nombre_Liga = Nombre;
+        this.Num_Equipos = Numero_Equipos;
+        this.Num_Ligas = Numero_ligas;
+        this.Federacion = Federacion;
+    }
+    
+    public void setID_Liga(String ID_Liga) {
+        this.ID_Liga = ID_Liga;
+    }
+
+    public void setNombre_Liga(String Nombre_Liga) {
+        this.Nombre_Liga = Nombre_Liga;
+    }
+
+    public void setNum_Equipos(String Num_Equipos) {
+        this.Num_Equipos = Num_Equipos;
+    }
+
+    public void setNum_Ligas(String Num_Ligas) {
+        this.Num_Ligas = Num_Ligas;
+    }
+
+    public void setFederacion(String Federacion) {
+        this.Federacion = Federacion;
+    }
+
+    public String getID_Liga() {
+        return ID_Liga;
+    }
+
+    public String getNombre_Liga() {
+        return Nombre_Liga;
+    }
+
+    public String getNum_Equipos() {
+        return Num_Equipos;
+    }
+
+    public String getNum_Ligas() {
+        return Num_Ligas;
+    }
+
+    public String getFederacion() {
+        return Federacion;
+    }
+    
+    public ArrayList DataBaseLeague(){
+        
+        ArrayList<Liga> vectorLeague = new ArrayList<Liga>();
+            Liga league1 = new Liga("LALIGA","LaLiga Santander","20","9", "Española");
+            vectorLeague.add(league1);
+            Liga league2 = new Liga("LIGUE1","Ligue 1","20","9","Francesa");
+            vectorLeague.add(league2);
+            Liga league3 = new Liga("SERIEA","Serie A","20","9","Italiana");
+            vectorLeague.add(league3);
+            Liga league4 = new Liga("BUNDESLIGA","Bundesliga","20","6","Alemana");
+            vectorLeague.add(league4);
+            Liga league5 = new Liga("PREMIER","Premier League","20","4","Inglesa");
+            vectorLeague.add(league5);
+            
+        return vectorLeague;
+    }
+}
